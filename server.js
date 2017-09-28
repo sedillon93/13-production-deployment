13ce50c6128dd2f4309c479be993f0a56ef8a591
Shannon Dillon <sedillon93@gmail.com>
2017-09-28 11:23:51 -0700

    refactoring ajax call to github api using proxy

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy'); // REVIEW: We've added a new package here to our requirements, as well as in the package.json
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = ''; // TODO: Don't forget to set your own conString
+const conString = process.env.DATABASE_URL || 'postgres://localhost:5432/blog_db'; // DONE: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
